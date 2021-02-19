7cb88a5d04a8c1e33de642511800d3943fe5559c
gitmrwick <chadwick.employment@gmail.com>
2021-02-19 17:28:15 +0100

    admin van teller_app models

--- balie_teller/teller_app/admin.py
+++ balie_teller/teller_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Balie, Tel
+
+class BalieInline(admin.StackedInline):
+    model = Balie
+    extra = 0
+
+@admin.register(Balie)
+class BalieAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Tel)
+class TelAdmin(admin.ModelAdmin):
+    inlines = [BalieInline,]
