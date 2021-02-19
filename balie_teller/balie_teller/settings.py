3419adc68c20025e263f53f830c921de199a2971
Chadwick <gitmrwick@users.noreply.github.com>
2021-02-19 12:06:55 +0100

    remove dev secret key

--- balie_teller/balie_teller/settings.py
+++ balie_teller/balie_teller/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!e&^v306%hc0@6siky6nw&7d5ophbjmk1ai5ql%p_v_%l1=1$9'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
